ad5c308bb7d2433a7dde31e22df3ce157fc8793e
Oscar Nilestam <oscarnilestam@gmail.com>
2019-06-07 16:38:57 +0200

    photo migration

--- db/migrate/20190605172818_add_imgurl_to_cocktails.rb
+++
@@ -1,5 +0,0 @@
-class AddImgurlToCocktails < ActiveRecord::Migration[5.2]
-  def change
-    add_column :cocktails, :imgurl, :string
-  end
-end
--- db/migrate/20190607094132_remove_imgurl_from_cocktails.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImgurlFromCocktails < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cocktails, :imgurl, :string
-  end
-end
